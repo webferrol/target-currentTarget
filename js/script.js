cc94c741bf116d1ad580ed0af01518bbacd7ef54
webferrol <xurxo@webferrol.com>
2020-02-17 19:24:43 +0100

    Modificación de currentTarget

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let op  = 1;
-let antiguo =null;
+let antiguo=null;
 let array =["El Elemento del DOM, que ha desencadenado un evento.","Siempre se refiere al elemento del event Handler (Manejador de Eventos) que ha sido atachado a un event.target especifico, este identifica el elemento en el que produjo el evento."];
 let script=['document.querySelector(".body").addEventListener("click",e=>{ e.target.classList.toggle("resaltado"); });',
 'document.querySelector(".body").addEventListener("click",e=>{ e.currentTarget.classList.toggle("resaltado"); });'];
@@ -44,6 +44,11 @@ document.querySelector("#selector").addEventListener("change",e=>{
     alert(array[op-1]);
 });
 
-document.querySelector(".pie_pagina").addEventListener("mouseout",e=>{
-    alert(e.relatedTarget.nodeName);
+document.querySelector(".pie_pagina").addEventListener("mouseover",e=>{
+    //alert(`mouseover => relatedTarget: ${e.relatedTarget.nodeName}, target: ${e.target.nodeName}, currentTarget: ${e.currentTarget.nodeName}`);
+    e.relatedTarget.classList.add("resaltado");
 });
+
+// document.querySelector("a").addEventListener("mouseout",e=>{
+//     //alert(`mouseout => relatedTarget: ${e.relatedTarget.nodeName}, target: ${e.target.nodeName}, currentTarget: ${e.currentTarget.nodeName}`);
+// });
